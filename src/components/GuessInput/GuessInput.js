2aacf8f8e92f289a158af19b9597db55c26cdab0
Widyarso Joko Purnomo <widyjp10@gmail.com>
2024-12-26 10:13:44 +0700

    exercise 1

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState("");
+
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log({ guess });
+        setGuess("");
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        required
+        minLength={5}
+        maxLength={5}
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={(event) => {
+          setGuess(event.target.value.toUpperCase());
+        }}
+        pattern="[A-Za-z]{5}"
+        title="5 letter word"
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
